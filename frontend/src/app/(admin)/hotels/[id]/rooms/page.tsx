30bc2ae907b3cb5c2af823fb1ed175e064ded429
ishva07 <ishvavisavadiya22@gmail.com>
2026-07-30 13:16:34 +0530

    fix: redirection of back button

--- frontend/src/app/(admin)/hotels/[id]/rooms/page.tsx
+++ frontend/src/app/(admin)/hotels/[id]/rooms/page.tsx
@@ -124,7 +124,7 @@ const handleFormSubmit = (formData: updateRoomFormInput) => {
       <Button
         variant="ghost"
         size="sm"
-        onClick={() => router.push(`/hotels/${hotelId}`)}
+        onClick={() => router.push(`/hotels`)}
       >
         <ArrowLeft className="h-4 w-4 mr-1" /> Back to Hotel
       </Button>
